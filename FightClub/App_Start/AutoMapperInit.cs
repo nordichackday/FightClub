2eee764933b05b886308999c04e3cb2a7d90f9aa
Bo Bendtsen <bben@dr.dk>
2015-05-28 23:27:43 +0200

    Added Match mapping

--- FightClub/App_Start/AutoMapperInit.cs
+++ FightClub/App_Start/AutoMapperInit.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using AutoMapper;
 using FightClub.Models;
+using Microsoft.Ajax.Utilities;
 
 namespace FightClub.App_Start
 {
@@ -12,6 +13,14 @@ namespace FightClub.App_Start
         public static void Init()
         {
             Mapper.CreateMap<Repository.user, User>();
+            Mapper.CreateMap<Repository.match, Match>()
+                .ForMember(a => a.User2Move1, b => b.MapFrom(c => (Move?) c.user2move1))
+                .ForMember(a => a.User2Move2, b => b.MapFrom(c => (Move?) c.user2move2))
+                .ForMember(a => a.User2Move3, b => b.MapFrom(c => (Move?) c.user2move3));
+
+#if DEBUG
+            Mapper.AssertConfigurationIsValid();
+#endif
         }
     }
 }
--- FightClub/Models/Match.cs
+++ FightClub/Models/Match.cs
@@ -7,5 +7,29 @@ namespace FightClub.Models
 {
     public class Match
     {
+        public int Id;
+        public bool Resolved;
+        public int Result;
+        public string User1;
+        public string User2;
+        public Move? User1Move1;
+        public Move? User1Move2;
+        public Move? User1Move3;
+        public Move? User2Move1;
+        public Move? User2Move2;
+        public Move? User2Move3;
+        public DateTime Created;
+        public DateTime? SolvedTime;
+        public int User1Points;
+        public int User2Points;
+    }
+    public enum Move
+    {
+        BlockHigh = 0,
+        HitHigh,
+        BlockMiddle,
+        HitMiddle,
+        BlockLow,
+        HitLow
     }
 }
