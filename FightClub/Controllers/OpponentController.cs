8e61d677b34c9848d75ad84663c8f12d010e20d2
Michael von der Fehr <mfeh@dr.dk>
2015-05-29 12:26:52 +0200

    modified api

--- FightClub/Controllers/OpponentController.cs
+++ FightClub/Controllers/OpponentController.cs
@@ -11,21 +11,25 @@ using FightClub.Repository.Implementation;
 
 namespace FightClub.Controllers
 {
+  [RoutePrefix("Opponent")]
     public class OpponentController : ApiController
     {
         private GameRepository _repo;
+
 
         public OpponentController()
         {
             _repo = new GameRepository();
         }
+       [Route("{username}")]
         [HttpGet]
-        //Api/Opponent?username=mfeh
+        //Opponent/mfeh
         public User GetOpponentByUserName(string username)
         {
            return AutoMapper.Mapper.Map<User>(_repo.GetOpponent(username));
         }
-        //Api/Opponent
+        [Route("Random/{username}")]
+        //Opponent/random/mfeh
         [HttpGet]
         public User GetRandomOpponent(string username)
         {
